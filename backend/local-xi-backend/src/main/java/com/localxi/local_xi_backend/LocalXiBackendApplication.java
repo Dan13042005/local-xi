e9e40e5301587b67d79bb954224cf41417e83759
Dan13042005 <Daniel2.Shipton@live.uwe.ac.uk>
2026-01-27 16:46:27 +0000

    Initialize Spring Boot backend project

---
+++ backend/local-xi-backend/src/main/java/com/localxi/local_xi_backend/LocalXiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localxi.local_xi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalXiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalXiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/local-xi-backend/src/test/java/com/localxi/local_xi_backend/LocalXiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localxi.local_xi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalXiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
